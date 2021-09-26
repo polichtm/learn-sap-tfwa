6681952d89707002c2ad2f7ea0b813cfbd47c31a
Your Name <polichtm@hotmail.com>
2021-09-26 10:08:35 -0400

    OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES \

--- deploy/modules/playbook-execution/main.tf
+++ deploy/modules/playbook-execution/main.tf
@@ -2,6 +2,7 @@
 resource null_resource "mount-disks-and-configure-hana" {
   provisioner "local-exec" {
     command = <<EOT
+    OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES \
     AZURE_RESOURCE_GROUPS="${var.az_resource_group}" \
     ANSIBLE_HOST_KEY_CHECKING="False" \
     ansible-playbook -u ${var.vm_user} \
